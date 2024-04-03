e1a1873b9647693039f2828294972884a65f63f7
weiezzhang <weiezzhang@tencent.com>
2024-04-03 17:24:11 +0800

    first commit

---
+++ contracts/Example.sol
@@ -0,0 +1,36 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.24;
+
+import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
+import "@openzeppelin/contracts/proxy/utils/UUPSUpgradeable.sol";
+// Uncomment this line to use console.log
+// import "hardhat/console.sol";
+
+contract Example is
+Initializable,
+OwnableUpgradeable,
+UUPSUpgradeable
+{
+    uint256 public a;
+
+    ///////////////////////////
+    //////// SYSTEM  /////////
+    ///////////////////////////
+    function initialize() public initializer {
+        __Ownable_init(msg.sender);
+    }
+
+    ///////////////////////////
+    ////////USER ACTION////////
+    ///////////////////////////
+
+
+
+    ///////////////////////////
+    //////// UPGRADE  /////////
+    ///////////////////////////
+
+    function _authorizeUpgrade(address newImplementation) internal onlyOwner override {
+    }
+}
